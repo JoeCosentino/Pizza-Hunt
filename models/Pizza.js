9b765566abe945f691b9397f4281c481495494ae
Joey Cos <joeycosentino24@gmail.com>
2022-06-24 16:02:13 -0400

    added comment model, began the controller for comments

--- models/Pizza.js
+++ models/Pizza.js
@@ -2,23 +2,42 @@ const { Schema, model } = require('mongoose');
 // setting the model equal to the variable 'Pizza' so that
 // we can appropriately export it to other files
 
-const PizzaSchema = new Schema({
-    pizzaName: {
-        type: String
+const PizzaSchema = new Schema(
+    {
+        pizzaName: {
+            type: String
+        },
+        createdBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        size: {
+            type: String,
+            default: 'Large'
+        },
+        toppings: [],
+        comments: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Comment'
+            }
+        ]
     },
-    createdBy: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    size: {
-        type: String,
-        default: 'Large'
-    },
-    toppings: []
-});
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
+
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.length
+})
 
 const Pizza = model('Pizza', PizzaSchema);
 
